9f3c6473a5c15b7379b2bcb2251ba184d0f407d2
Jorge <jorgeanton1810@gmail.com>
2025-02-18 12:45:22 +0100

    Primer commit - Proyecto Spring Boot

---
+++ redproducer/src/main/java/com/redproducer/RedproducerApplication.java
@@ -0,0 +1,13 @@
+package com.redproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedproducerApplication.class, args);
+	}
+
+}
---
+++ redproducer/src/test/java/com/redproducer/RedproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
