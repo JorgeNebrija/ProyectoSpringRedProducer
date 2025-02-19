6710df725ccc584a48639419fe221ed8ee1528af
Jorge <jorgeanton1810@gmail.com>
2025-02-19 10:28:45 +0100

    Crear models usuario para el funcionamiento login

---
+++ redproducer/src/main/java/com/redproducer/models/Usuario.java
@@ -0,0 +1,19 @@
+package com.redproducer.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
