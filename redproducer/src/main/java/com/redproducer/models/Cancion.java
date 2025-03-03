23fd2cb086323c02f710c5d4f38240dc2407841f
=Samuel_Navasardyan <=sur00044@gmail.com>
2025-03-03 16:52:52 +0100

    Arreglar conflicto spring

--- redproducer/src/main/java/com/redproducer/models/Cancion.java
+++ redproducer/src/main/java/com/redproducer/models/Cancion.java
@@ -3,89 +3,91 @@ package com.redproducer.models;
 import jakarta.persistence.*;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 @Table(name = "canciones")
 
 public class Cancion {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String titulo;
-    private String portada;
-    private String audio;
-    private int duracion;
-    private int delay;
+	private String titulo;
+	private String portada;
+	private String audio;
+	private int duracion;
+	private int delay;
 
-    @ManyToOne
-    @JoinColumn(name = "id_artista")
-    private Artista artista;
+	@ManyToOne
+	@JoinColumn(name = "id_artista")
+	private Artista artista;
 
-    @OneToMany(mappedBy = "cancion", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Letra> letra;
+	@OneToMany(mappedBy = "cancion", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Letra> letra;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public String getPortada() {
-        return portada;
-    }
+	public String getPortada() {
+		return portada;
+	}
 
-    public void setPortada(String portada) {
-        this.portada = portada;
-    }
+	public void setPortada(String portada) {
+		this.portada = portada;
+	}
 
-    public String getAudio() {
-        return audio;
-    }
+	public String getAudio() {
+		return audio;
+	}
 
-    public void setAudio(String audio) {
-        this.audio = audio;
-    }
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
 
-    public int getDuracion() {
-        return duracion;
-    }
+	public int getDuracion() {
+		return duracion;
+	}
 
-    public void setDuracion(int duracion) {
-        this.duracion = duracion;
-    }
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
 
-    public int getDelay() {
-        return delay;
-    }
+	public int getDelay() {
+		return delay;
+	}
 
-    public void setDelay(int delay) {
-        this.delay = delay;
-    }
+	public void setDelay(int delay) {
+		this.delay = delay;
+	}
 
-    public Artista getArtista() {
-        return artista;
-    }
+	public Artista getArtista() {
+		return artista;
+	}
 
-    public void setArtista(Artista artista) {
-        this.artista = artista;
-    }
+	public void setArtista(Artista artista) {
+		this.artista = artista;
+	}
 
-    public List<Letra> getLetra() {
-        return letra;
-    }
+	public List<Letra> getLetra() {
+		return letra;
+	}
 
-    public void setLetra(List<Letra> letra) {
-        this.letra = letra;
-    }
+	public void setLetra(List<Letra> letra) {
+		this.letra = letra;
+	}
 }
 
