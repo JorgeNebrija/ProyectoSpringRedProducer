d60254e53103572b2f09366390474616a0614a99
=Samuel_Navasardyan <=sur00044@gmail.com>
2025-03-03 17:04:53 +0100

    Arreglo del conflicto

--- redproducer/src/main/java/com/redproducer/models/Cancion.java
+++ redproducer/src/main/java/com/redproducer/models/Cancion.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 @Table(name = "canciones")
 public class Cancion {
@@ -19,37 +17,74 @@ public class Cancion {
 	private int duracion;
 	private int delay;
 
-    @ManyToOne
-    @JoinColumn(name = "id_artista")
-    @JsonIgnoreProperties("canciones") // Evita recursión infinita en Artista
-    private Artista artista;
+	@ManyToOne
+	@JoinColumn(name = "id_artista")
+	@JsonIgnoreProperties("canciones") // Evita recursión infinita en Artista
+	private Artista artista;
 
-    @OneToMany(mappedBy = "cancion", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-@JsonIgnoreProperties("cancion") // Evita recursión infinita
-private List<Letra> letra;
+	@OneToMany(mappedBy = "cancion", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonIgnoreProperties("cancion") // Evita recursión infinita
+	private List<Letra> letra;
 
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitulo() { return titulo; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public String getPortada() { return portada; }
-    public void setPortada(String portada) { this.portada = portada; }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public String getAudio() { return audio; }
-    public void setAudio(String audio) { this.audio = audio; }
+	public String getPortada() {
+		return portada;
+	}
 
-    public int getDuracion() { return duracion; }
-    public void setDuracion(int duracion) { this.duracion = duracion; }
+	public void setPortada(String portada) {
+		this.portada = portada;
+	}
 
-    public int getDelay() { return delay; }
-    public void setDelay(int delay) { this.delay = delay; }
+	public String getAudio() {
+		return audio;
+	}
 
-    public Artista getArtista() { return artista; }
-    public void setArtista(Artista artista) { this.artista = artista; }
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
 
-    public List<Letra> getLetra() { return letra; }
-    public void setLetra(List<Letra> letra) { this.letra = letra; }
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public int getDelay() {
+		return delay;
+	}
+
+	public void setDelay(int delay) {
+		this.delay = delay;
+	}
+
+	public Artista getArtista() {
+		return artista;
+	}
+
+	public void setArtista(Artista artista) {
+		this.artista = artista;
+	}
+
+	public List<Letra> getLetra() {
+		return letra;
+	}
+
+	public void setLetra(List<Letra> letra) { this.letra=letra;}
 }
